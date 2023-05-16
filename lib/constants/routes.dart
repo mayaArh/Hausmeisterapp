3fa62c0834756fb5766d9b29f802805a8bb72d1f
mayaArh <104135513+mayaArh@users.noreply.github.com>
2023-05-16 14:36:18 +0200

    Revert "add ticket ui start"
    This reverts commit 8fe5fdc62d825711781da4925460129a4c7a3604.

--- lib/constants/routes.dart
+++ lib/constants/routes.dart
@@ -1,5 +1,4 @@
 const loginRoute = '/login/';
 const registerRoute = '/register/';
 const renterHomeRoute = '/renter_home/';
-const verifyEmailRoute = '/verify_email/';
-const newTicketRoute = 'renter_views/new_ticket/';
+const verifyEmailRoute = '/verify-email/';
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:mein_digitaler_hausmeister/services/auth/auth_service.dart';
-import 'package:mein_digitaler_hausmeister/views/renter_views/create_ticket.dart';
 import 'constants/routes.dart';
 import 'views/login_view.dart';
 import 'views/register_view.dart';
-import 'views/renter_views/renter_tickets_overview.dart';
+import 'views/renter_tickets_overview.dart';
 import 'views/verify_email_view.dart';
 import 'dart:developer' as developer;
 
@@ -21,8 +20,7 @@ void main() {
           loginRoute: (context) => const LoginView(),
           registerRoute: (context) => const RegisterView(),
           renterHomeRoute: (context) => const RenterTicketOverview(),
-          verifyEmailRoute: (context) => const VerifyEmailView(),
-          newTicketRoute: (context) => const TicketCreationView(),
+          verifyEmailRoute: (context) => const VerifyEmailView()
         }),
   );
 }
